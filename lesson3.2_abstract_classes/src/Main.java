7e2c0e73b904a2d8ef6ae2e9aad31ccb27f64c95
admin <admin@MacBook-Pro-admin.local>
2024-02-13 17:30:35 +0300

    Fixed Main method, added List

--- lesson3.2_abstract_classes/src/Main.java
+++ lesson3.2_abstract_classes/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     /*
         Создать абстрактный класс Фрукт и классы Яблоко, Груша, Абрикос расширяющие его.
@@ -11,11 +14,21 @@ public class Main {
      */
 
     public static void main(String[] args) {
+        List<Fruit> fruit = new ArrayList<>();
+
         Apple apple = new Apple(1.5);
         Pear pear = new Pear(2);
         Apricot apricot = new Apricot(3.5);
 
-        double totalCost = apple.getCost() + pear.getCost() + apricot.getCost();
+        fruit.add(apple);
+        fruit.add(pear);
+        fruit.add(apricot);
+
+        double totalCost = 0;
+
+        for (Fruit fruit1 : fruit) {
+            totalCost += fruit1.getCost();
+        }
         System.out.println("Total cost of fruits cost - " + totalCost);
     }
 }
